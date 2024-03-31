a3e0c49f695e50c175baffcfc0d9d66783261fc8
Marcelo Junior <dbvmarcelo13@gmail.com>
2024-03-31 20:09:09 -0300

    add: item menu active

--- src/Components/SideMenu/index.tsx
+++ src/Components/SideMenu/index.tsx
@@ -1,26 +1,43 @@
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { BsSuitcaseLg } from "react-icons/bs";
 import { FaRegUser } from "react-icons/fa";
 import { TiContacts } from "react-icons/ti";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function SideMenu() {
     const { t } = useTranslation();
+    const [active, setActive] = useState<'about' | 'projects' | 'contact'>('about');
+
+    const location = useLocation();
+
+    useEffect(() => {
+        var path = location.pathname;
+
+        if(path === '/') {
+            setActive('about');
+        } else if(path === '/projects') {
+            setActive('projects');
+        } else if(path === '/contact') {
+            setActive('contact');
+        }
+    }, [location]);
+
     return (
         <div className="bg-white p-4 rounded-lg flex flex-col gap-3">
-            <Link to="/" className="item-menu-side">
+            <Link to="/" className={`item-menu-side ${active === 'about' ? 'bg-primary-blue text-white' : 'bg-white'}`}>
                 <FaRegUser size={20} />
                 <span>
                     {t('about')}
                 </span>
             </Link>
-            <Link to="/projects" className="item-menu-side">
+            <Link to="/projects" className={`item-menu-side ${active === 'projects' ? 'bg-primary-blue text-white' : 'bg-white'}`}>
                 <BsSuitcaseLg size={20} />
                 <span>
                     Projects
                 </span>
             </Link>
-            <Link to="/contact" className="item-menu-side">
+            <Link to="/contact" className={`item-menu-side ${active === 'contact' ? 'bg-primary-blue text-white' : 'bg-white'}`}>
                 <TiContacts size={20} />
                 <span>
                     Contato
--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -5,7 +5,7 @@ import SwitchLanguage from "../../Components/SwitchLanguage";
 
 export default function DefaultLayout() {
     return (
-        <div className="bg-shiny h-screen">
+        <div className="">
             <div className="flex flex-col gap-4 container mx-auto pt-28 pb-4 px-4 md:px-0">
                 <div className="w-full flex justify-end">
                     <SwitchLanguage />
