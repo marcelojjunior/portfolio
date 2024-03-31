e073f86f0c2e63421c79acb7807968866928fd5b
Marcelo Junior <dbvmarcelo13@gmail.com>
2024-03-31 18:59:39 -0300

    add: font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      comfortaa: ["Comfortaa", "sans-serif"],
+    },
     extend: {
       backgroundImage: {
         'shiny': "url('./src/assets/images/background.svg')",
