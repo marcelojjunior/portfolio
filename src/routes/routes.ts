3754fb44c9737d938aaee2e9f360748dca4246bf
marcelojjunior <dbvmarcelo13@gmail.com>
2025-02-27 13:55:27 -0300

    clear project

--- src/routes/routes.ts
+++
@@ -1,4 +0,0 @@
-export { default as Home } from '../pages/Home';
-export { default as Projects } from '../pages/Projects';
-export { default as Contact } from '../pages/Contact';
-export { default as Resume } from '../pages/Resume';
